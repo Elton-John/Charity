35c3762bc65d4fe52922e72592daac3087834e7c
Olga <nauka.dinosaurus@gmail.com>
2020-12-19 09:54:36 +0100

    Add repositories

--- src/main/java/pl/coderslab/charity/institution/InstitutionRepository.java
+++ src/main/java/pl/coderslab/charity/institution/InstitutionRepository.java
@@ -1,7 +1,9 @@
 package pl.coderslab.charity.institution;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import pl.coderslab.charity.model.Institution;
 
 @Repository
-public class InstitutionRepository {
+public interface InstitutionRepository extends JpaRepository<Institution, Long> {
 }
