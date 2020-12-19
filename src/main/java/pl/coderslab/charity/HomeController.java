b92ce2464024ed78fc4a23b66ba6ba90074fcd81
Olga <nauka.dinosaurus@gmail.com>
2020-12-19 11:27:25 +0100

    Add list of all institutions for landing page

--- src/main/java/pl/coderslab/charity/HomeController.java
+++ src/main/java/pl/coderslab/charity/HomeController.java
@@ -1,16 +1,31 @@
 package pl.coderslab.charity;
 
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pl.coderslab.charity.institution.InstitutionRepository;
+import pl.coderslab.charity.model.Institution;
+
+import java.util.List;
 
 
 @Controller
+@AllArgsConstructor
 public class HomeController {
+    private InstitutionRepository institutionRepository;
 
 
     @RequestMapping("/")
-    public String homeAction(Model model){
+        public String homeAction(Model model){
+        model.addAttribute("institutions");
         return "index";
+    }
+
+
+    @ModelAttribute("institutions")
+    public List<Institution> institutions(){
+        return institutionRepository.findAll();
     }
 }
---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO institution  (name, description) VALUES ('Fundacja "Dbam o Zdrowie"', 'Cel i misja: Pomoc dzieciom z ubogich rodzin.');
+INSERT INTO institution  (name, description) VALUES ('Fundacja "A kogo"', 'Cel i misja: Pomoc wybudzaniu dzieci ze śpiączki.');
+INSERT INTO institution  (name, description) VALUES ('Fundacja “Dla dzieci"', 'Cel i misja:Pomoc osobom znajdującym się w trudnej sytuacji życiowej.');
+INSERT INTO institution  (name, description) VALUES ('Fundacja “Bez domu”', 'Cel i misja: Pomoc dla osób nie posiadających miejsca zamieszkania');
