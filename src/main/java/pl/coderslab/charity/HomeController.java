818628c9a169f8106ccfed66977f80f16c77bfa4
Olga <nauka.dinosaurus@gmail.com>
2020-12-19 12:11:45 +0100

    Add total number of all donation bags for landing page

--- src/main/java/pl/coderslab/charity/HomeController.java
+++ src/main/java/pl/coderslab/charity/HomeController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pl.coderslab.charity.donation.DonationRepository;
 import pl.coderslab.charity.institution.InstitutionRepository;
 import pl.coderslab.charity.model.Institution;
 
@@ -15,17 +16,19 @@ import java.util.List;
 @AllArgsConstructor
 public class HomeController {
     private InstitutionRepository institutionRepository;
+    private DonationRepository donationRepository;
 
 
     @RequestMapping("/")
-        public String homeAction(Model model){
+    public String homeAction(Model model) {
         model.addAttribute("institutions");
+        model.addAttribute("quantity", donationRepository.countAllQuantity());
         return "index";
     }
 
 
     @ModelAttribute("institutions")
-    public List<Institution> institutions(){
+    public List<Institution> institutions() {
         return institutionRepository.findAll();
     }
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,3 +2,5 @@ INSERT INTO institution  (name, description) VALUES ('Fundacja "Dbam o Zdrowie"'
 INSERT INTO institution  (name, description) VALUES ('Fundacja "A kogo"', 'Cel i misja: Pomoc wybudzaniu dzieci ze śpiączki.');
 INSERT INTO institution  (name, description) VALUES ('Fundacja “Dla dzieci"', 'Cel i misja:Pomoc osobom znajdującym się w trudnej sytuacji życiowej.');
 INSERT INTO institution  (name, description) VALUES ('Fundacja “Bez domu”', 'Cel i misja: Pomoc dla osób nie posiadających miejsca zamieszkania');
+INSERT INTO donation (quantity) VALUES ('5');
+INSERT INTO donation (quantity) VALUES ('15');
